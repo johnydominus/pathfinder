875799a6613562adb8a2730de3e1bd7425bbf64e
JDominus <johnydsound@gmail.com>
2019-12-02 21:51:12 +0200

    CHANGE THIS FOR MAC!

--- libmx/src/mx_realloc.c
+++ libmx/src/mx_realloc.c
@@ -5,15 +5,15 @@ void *mx_realloc(void *ptr, size_t size) {
 
     if (!ptr)
         return malloc(size);
-    else if (malloc_usable_size(ptr) >= size) 
+    else if (malloc_usable_size(ptr) >= size)               //change function for "malloc_size(ptr) 
         return ptr;
     else if (size == 0) {
         free(ptr);
-        return malloc(malloc_usable_size(NULL));
+        return malloc(malloc_usable_size(NULL));            //and here!
     }
     else {
         newPtr = malloc(size);
-        mx_memcpy(newPtr, ptr, malloc_usable_size(ptr));
+        mx_memcpy(newPtr, ptr, malloc_usable_size(ptr));    //and here!
         free(ptr);
     }
 
