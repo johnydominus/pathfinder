18ea55be59d67a09149957e6ddb6e3366a2bd66c
JDominus <johnydsound@gmail.com>
2020-01-05 21:08:20 +0200

    all errors(exc. islands num) && parsing (at beginning)

--- pathfinder/mx_create_vertice.c
+++
@@ -1,9 +0,0 @@
-#include "pathfinder.h"
-
-t_vertice *mx_create_vertice(const char *name) {
-    t_vertice *new_vertice = malloc(sizeof(t_vertice));
-    new_vertice->name = mx_strdup(name);
-    new_vertice->edges = NULL;
-
-    return new_vertice;
-}
